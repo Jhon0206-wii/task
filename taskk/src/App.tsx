221932ca078fa98aa94cf0d66009fd9373f7ef7e
Jhon Aguilar Gomez <jhonsaguilar2006@gmail.com>
2026-07-27 20:28:09 -0500

    crear funciones pero se quedaron a medias sin terminar

--- taskk/src/App.tsx
+++ taskk/src/App.tsx
@@ -1,24 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabase";
 
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
+  created_at: string;
+}
+
 function App() {
+
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState("");
+
+  const obtenerTareas = async () => {
+    const { data, error } = await supabase.from("tasks").select("*");
+
+    if (error) {
+      setError("No se pudieron cargar las tareas");
+      console.log(error);
+      return;
+    }
+
+    setTasks(data);
+  };
+
   useEffect(() => {
-    const obtenerTareas = async () => {
-      const { data, error } = await supabase
-        .from("tasks")
-        .select("*");
-
-      if (error) {
-        console.error("❌ Error:", error);
-      } else {
-        console.log("✅ Datos:", data);
-      }
-    };
-
     obtenerTareas();
   }, []);
 
-  return <h1>Conexión con Supabase</h1>;
-}
+  const agregarTarea = async () => {}
+  
 
+  return <div>lista de tareas</div>;
+}
 export default App;
